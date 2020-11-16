34b324fae9d715090c0828c6b988a58760eb7be5
LikeGoToLF <1143030699@qq.com>
2020-11-16 20:19:27 +0800

    review1

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 let checkList = document.getElementById('checkList')
 let liArr = checkList.querySelectorAll('li')
-console.log(liArr)
+let inputs = checkList.querySelectorAll('input')
+for (let i = 0; i < liArr.length; i++) {
+  liArr[i].addEventListener('click', edit)
+  inputs[i].addEventListener('blur', change)
+  inputs[i].addEventListener('keypress', handle)
+}
+function edit() {
+  let input = this.querySelector('input')
+  this.className = 'edit'
+  input.focus()
+  input.setSelectionRange(0, input.value.length)
+}
+function change() {
+  this.previousElementSibling.innerHTML = this.value
+  this.parentNode.className = ''
+}
+function handle(event) {
+  if (event.which === 13) {
+    change.call(this)
+  }
+}
